140e1810c0651ec5e32d8b767bc0049c0dee3052
QihaoYe <11610812@mail.sustc.edu.cn>
2017-09-27 00:14:15 +0800

    update

--- Main.py
+++ Main.py
@@ -8,12 +8,37 @@ import numpy as np
 import pandas as pd
 
 
+# def get_each_value(array, i):
+#     string = str(array.ix[i].values).replace('[', '').replace(']', '')
+#     string = string.replace('\'', '', 3).replace('\'', '', -1).split(' ', 2)
+#     return string
+
+
 PATH = './Lib/Alphabet.csv'
-alphabet = pd.read_csv(PATH)
-alphabet = alphabet.ix[0]
+alphabet = pd.read_csv(PATH).values
+LENGTH = len(alphabet)
 
-# while 1:
-    # word = input('Your word: ')
-word = 'Abundant'
-print(alphabet)
 
+def find_word_index(word):
+    for i in range(LENGTH):
+        if alphabet[i, 1].lower() == word.lower():
+            return i
+    return -1
+
+
+while 1:
+    word = input('Your word: ')
+    if word in ['q', 'quit', 'exit']:
+        break
+    index = find_word_index(word)
+    if index is -1:
+        continue
+    definition = input('Your definition: ')
+    print()
+    print('Your definition: %s' % definition)
+    print('Real definition: %s' % alphabet[index, 2])
+    if alphabet[index, 2] == definition:
+        print('Exactly!')
+    else:
+        print('Not so good!')
+    
