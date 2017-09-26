3a0c2f5d6846ef5350665f3810f6d51f9ec8e139
QihaoYe <11610812@mail.sustc.edu.cn>
2017-09-26 23:47:02 +0800

    update

--- Main.py
+++ Main.py
@@ -3,3 +3,17 @@
 __author__ = 'Yee_172'
 __data__ = '2017/9/26'
 
+
+import numpy as np
+import pandas as pd
+
+
+PATH = './Lib/Alphabet.csv'
+alphabet = pd.read_csv(PATH)
+alphabet = alphabet.ix[0]
+
+# while 1:
+    # word = input('Your word: ')
+word = 'Abundant'
+print(alphabet)
+
