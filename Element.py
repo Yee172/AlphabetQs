8359243aa0772a167b3dfaf57138d2b516338559
QihaoYe <11610812@mail.sustc.edu.cn>
2017-09-29 11:35:40 +0800

    update

--- Element.py
+++ Element.py
@@ -4,7 +4,7 @@ __author__ = 'Yee_172'
 __data__ = '2017/9/27'
 
 
-import numpy as np
+# import numpy as np
 
 
 class Word:
@@ -15,7 +15,35 @@ class Word:
     def __init__(self, line):
         self.family = line[1][0].upper()
         self.num = int(line[0])
-        self.word = line[1].lower()
-        self.definition = line[2].lower()
-        self.sample = line[3].lower()
-        total += 1
+        self.word = line[1]
+        self.definition = line[2]
+        self.sample = line[3]
+        Word.total += 1
+
+    def get_sample(self):
+        return self.sample.replace('``', '\033[1;31m').replace('`', '\033[0m')
+
+
+class Family:
+    """
+    A class of a group of words
+    """
+    def __init__(self, letter, words):
+        self.letter = letter.upper()
+        self.words = [word for word in words if word.family == self.letter]
+
+
+class Alphabet:
+    """
+    A class of Alphabet
+    """
+    def __init__(self, words):
+        self.familys = {'A': Family('A', words), 'B': Family('B', words), 'C': Family('C', words),\
+                        'D': Family('D', words), 'E': Family('E', words), 'F': Family('F', words),\
+                        'G': Family('G', words), 'H': Family('H', words), 'I': Family('I', words),\
+                        'J': Family('J', words), 'K': Family('K', words), 'L': Family('L', words),\
+                        'M': Family('M', words), 'N': Family('N', words), 'O': Family('O', words),\
+                        'P': Family('P', words), 'Q': Family('Q', words), 'R': Family('R', words),\
+                        'S': Family('S', words), 'T': Family('T', words), 'U': Family('U', words),\
+                        'V': Family('V', words), 'W': Family('W', words), 'X': Family('X', words),\
+                        'Y': Family('Y', words), 'Z': Family('Z', words)}
--- Func.py
+++ Func.py
@@ -7,6 +7,7 @@ __all__ = ['read_in']
 
 import numpy as np
 import pandas as pd
+from Element import Word, Family, Alphabet
 
 PATH = './Lib/Alphabet.csv'
 
@@ -15,3 +16,9 @@ def read_in(csvfile):
     return pd.read_csv(csvfile).values
 
 
+# ---[test zone]---
+alphabet = read_in(PATH)
+words = [Word(x) for x in alphabet]
+FamilyA = Family('A', words)
+alphabet = Alphabet(words)
+print(alphabet.familys['A'].words)
