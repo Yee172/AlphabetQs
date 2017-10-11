4e4412532669fb0a1c5b3b20579498d0bb1a3b69
QihaoYe <11610812@mail.sustc.edu.cn>
2017-10-11 23:28:19 +0800

    update

--- Main.py
+++ Main.py
@@ -4,21 +4,22 @@ __author__ = 'Yee_172'
 __data__ = '2017/9/26'
 
 
-# from Func import *
-import sys
-from PyQt5 import QtWidgets
-from GUI.main import Ui_Dialog
+from Func import *
+# import sys
+# from PyQt5 import QtWidgets
+# from GUI.main import Ui_Dialog
+#
+#
+# class MainWin(QtWidgets.QWidget, Ui_Dialog):
+#     def __init__(self):
+#         super(MainWin, self).__init__()
+#         self.setupUi(self)
+#
+#
+# if __name__ == '__main__':
+#     app = QtWidgets.QApplication(sys.argv)
+#     win = MainWin()
+#     win.show()
+#     sys.exit(app.exec_())
 
-
-class MainWin(QtWidgets.QWidget, Ui_Dialog):
-    def __init__(self):
-        super(MainWin, self).__init__()
-        self.setupUi(self)
-
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    win = MainWin()
-    win.show()
-    sys.exit(app.exec_())
-
+terminal_version_old()
