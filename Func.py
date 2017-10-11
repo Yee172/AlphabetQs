50caeebada3714049b1d098e50a059c15ea8b6b9
QihaoYe <11610812@mail.sustc.edu.cn>
2017-10-11 20:20:26 +0800

    update

--- Func.py
+++ Func.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 __author__ = 'Yee_172'
 __data__ = '2017/9/26'
-__all__ = ['find_word', 'clear_screen', 'random_word', 'definition_q']
+__all__ = ['find_word', 'clear_screen', 'random_word', 'definition_q', 'terminal_version_old']
 
 
 import os
@@ -53,6 +53,53 @@ def definition_q(word):
     print()
     print('Sample sentence: %s' % word.get_sample())
     print('\n' * 17)
+
+
+def terminal_version_old():
+    MODE = 'SEARCH'
+    RANDOM_LETTER = 'all'
+    print('Initail mode is SEARCH mode!')
+    while 1:
+        if MODE.lower() == 'search':
+            print()
+            content = input('Your word or num: ')
+            if content.lower() in ['q', 'quit', 'exit']:
+                break
+            if content.lower() in ['r', 'rand', 'random']:
+                clear_screen()
+                print('You are in RANDOM mode now!')
+                MODE = 'RANDOM'
+                continue
+            try:
+                content = int(content)
+            except:
+                pass
+            word = find_word(content)
+            if word is None:
+                continue
+            print('\nYour word:       %s' % word.word)
+            definition_q(word)
+        if MODE.lower() == 'random':
+            print()
+            content = input('Your random letter: ')
+            if content.lower() in ['q', 'quit', 'exit']:
+                break
+            if content.lower() in ['s', 'sear', 'search']:
+                clear_screen()
+                print('You are in SEARCH mode now!')
+                MODE = 'SEARCH'
+                continue
+            try:
+                word = random_word(content)
+                if word is None:
+                    print('Word not available! Rolling back!')
+                    raise
+                RANDOM_LETTER = content
+            except:
+                word = random_word(RANDOM_LETTER)
+            print('        #      : %d' % word.num)
+            print('Your word      : %s' % word.word)
+            definition_q(word)
 
 
 # ---[test zone]---
--- Main.py
+++ Main.py
@@ -4,50 +4,8 @@ __author__ = 'Yee_172'
 __data__ = '2017/9/26'
 
 
-from Func import *
-MODE = 'SEARCH'
-RANDOM_LETTER = 'all'
+# from Func import *
+from GUI.main import Ui_Dialog
 
 
-print('Initail mode is SEARCH mode!')
-while 1:
-    if MODE.lower() == 'search':
-        print()
-        content = input('Your word or num: ')
-        if content.lower() in ['q', 'quit', 'exit']:
-            break
-        if content.lower() in ['r', 'rand', 'random']:
-            clear_screen()
-            print('You are in RANDOM mode now!')
-            MODE = 'RANDOM'
-            continue
-        try:
-            content = int(content)
-        except:
-            pass
-        word = find_word(content)
-        if word is None:
-            continue
-        print('\nYour word:       %s' % word.word)
-        definition_q(word)
-    if MODE.lower() == 'random':
-        print()
-        content = input('Your random letter: ')
-        if content.lower() in ['q', 'quit', 'exit']:
-            break
-        if content.lower() in ['s', 'sear', 'search']:
-            clear_screen()
-            print('You are in SEARCH mode now!')
-            MODE = 'SEARCH'
-            continue
-        try:
-            word = random_word(content)
-            if word is None:
-                print('Word not available! Rolling back!')
-                raise
-            RANDOM_LETTER = content
-        except:
-            word = random_word(RANDOM_LETTER)
-        print('        #      : %d' % word.num)
-        print('Your word      : %s' % word.word)
-        definition_q(word)
+# terminal_version_old()
