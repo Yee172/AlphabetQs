d28f610ea282e31fd7f43ccfc4012f3a1503d7da
QihaoYe <11610812@mail.sustc.edu.cn>
2017-10-16 01:19:24 +0800

    update

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ __data__ = '2017/9/26'
 
 from Func import *
 import sys
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QCoreApplication
 from PyQt5 import QtGui
 from PyQt5 import QtWidgets
 from GUI.main import Ui_Dialog
@@ -20,17 +20,30 @@ class MainWin(QtWidgets.QWidget, Ui_Dialog):
     def __init__(self):
         super(MainWin, self).__init__()
         self.setupUi(self)
-        self.wordlist_operate()
-        self.button_show_wordlist.clicked.connect(self.wordlist.collapseAll)
+        self.button_show_wordlist.clicked.connect(self.wordlist_click)
 
-    def wordlist_operate(self):
+    def wordlist_click(self):
+        content = self.button_show_wordlist.text()
+        _translate = QCoreApplication.translate
+        if content == 'SHOW':
+            self.wordlist_show()
+            self.button_show_wordlist.setText(_translate("Dialog", "HIDE"))
+        if content == 'HIDE':
+            self.wordlist_hide()
+            self.button_show_wordlist.setText(_translate("Dialog", "SHOW"))
+
+    def wordlist_show(self):
         wordlist_model = QtGui.QStandardItemModel(0, 2, self)
         wordlist_model.setHeaderData(0, Qt.Horizontal, '#')
         wordlist_model.setHeaderData(1, Qt.Horizontal, 'WORD')
-        self.wordlist.setModel(self.EMPTY_MODEL)
-        # self.wordlist.setColumnWidth(0, 30)
+        self.wordlist.setModel(wordlist_model)
+        self.wordlist.setColumnWidth(0, 30)
         for each in alphabet.words:
             self.add_data(wordlist_model, each.num, each.word)
+
+    def wordlist_hide(self):
+        self.wordlist.setModel(self.EMPTY_MODEL)
+        self.WORDLIST_NUM = 0
 
     def add_data(self, model, num, word):
         model.insertRow(self.WORDLIST_NUM)
