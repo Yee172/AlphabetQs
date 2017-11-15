3727207ccf13e02cf9a1f032e1ff13146c2ea597
QihaoYe <11610812@mail.sustc.edu.cn>
2017-11-15 20:33:10 +0800

    update

--- Element.py
+++ Element.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 __author__ = 'Yee_172'
-__data__ = '2017/9/27'
+__date__ = '2017/9/27'
 __all__ = ['Word', 'Family', 'Alphabet']
 
 
--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 __author__ = 'Yee_172'
-__data__ = '2017/9/26'
+__date__ = '2017/9/26'
 
 
 from Func import *
